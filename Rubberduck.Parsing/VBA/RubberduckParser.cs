dcceff9ea94ef7136006521f38005bfee775416a
Matt Collins <matt@mjolka.com>
2015-11-17 14:51:24 +1100

    Remove dependency on ParseCoordinator

--- Rubberduck.Parsing/VBA/RubberduckParser.cs
+++ Rubberduck.Parsing/VBA/RubberduckParser.cs
@@ -21,14 +21,12 @@ namespace Rubberduck.Parsing.VBA
     public class RubberduckParser : IRubberduckParser
     {
         private readonly VBE _vbe;
-        private readonly ParseCoordinator _coordinator;
         private readonly Logger _logger;
 
         public RubberduckParser(VBE vbe)
         {
             _vbe = vbe;
             _logger = LogManager.GetCurrentClassLogger();
-            _coordinator = new ParseCoordinator((component, state) => State.SetModuleState(component, state));
         }
 
         private readonly RubberduckParserState _state = new RubberduckParserState();
@@ -92,12 +90,25 @@ namespace Rubberduck.Parsing.VBA
             return parser.startRule();
         }
 
-        private Tuple<IParseTree, ITokenStream, Action> Parse(VBComponent component, IEnumerable<IParseTreeListener> listeners, CancellationToken token)
+        private class ParseResult
+        {
+            public ParseResult(IParseTree parseTree, ITokenStream tokenStream)
+            {
+                ParseTree = parseTree;
+                TokenStream = tokenStream;
+            }
+
+            public IParseTree ParseTree { get; }
+
+            public ITokenStream TokenStream { get; }
+        }
+
+        private ParseResult Parse(VBComponent component, IEnumerable<IParseTreeListener> listeners)
         {
             ITokenStream stream;
             var code = component.CodeModule.Lines();
             var tree = Parse(code, listeners, out stream);
-            return Tuple.Create(tree, stream, new Action(() => { ResolveReferences(tree, token); }));
+            return new ParseResult(tree, stream);
         }
 
         public void Parse(VBE vbe)
@@ -115,11 +126,6 @@ namespace Rubberduck.Parsing.VBA
 
         public void Parse(VBComponent vbComponent)
         {
-            _coordinator.Start(vbComponent, () => ParseInternal(vbComponent));
-        }
-
-        private Action ParseInternal(VBComponent vbComponent)
-        {
             var token = CancelPreviousTask(vbComponent);
             _state.ClearDeclarations(vbComponent);
 
@@ -136,7 +142,7 @@ namespace Rubberduck.Parsing.VBA
             };
 
             _state.Status = RubberduckParserState.State.Parsing;
-            var result = Parse(vbComponent, listeners, token);
+            var result = Parse(vbComponent, listeners);
 
             // cannot locate declarations in one pass *the way it's currently implemented*,
             // because the context in EnterSubStmt() doesn't *yet* have child nodes when the context enters.
@@ -146,15 +152,15 @@ namespace Rubberduck.Parsing.VBA
             declarationsListener.NewDeclaration += declarationsListener_NewDeclaration;
             declarationsListener.CreateModuleDeclarations();
             var walker = new ParseTreeWalker();
-            walker.Walk(declarationsListener, result.Item1);
+            walker.Walk(declarationsListener, result.ParseTree);
             declarationsListener.NewDeclaration -= declarationsListener_NewDeclaration;
 
             _state.ObsoleteCallContexts = obsoleteCallsListener.Contexts.Select(context => new QualifiedContext(qualifiedName, context));
             _state.ObsoleteLetContexts = obsoleteLetListener.Contexts.Select(context => new QualifiedContext(qualifiedName, context));
 
-            _state.AddTokenStream(vbComponent, result.Item2);
+            _state.AddTokenStream(vbComponent, result.TokenStream);
 
-            return result.Item3;
+            ResolveReferences(result.ParseTree, token);
         }
 
         private void declarationsListener_NewDeclaration(object sender, DeclarationEventArgs e)
@@ -189,20 +195,17 @@ namespace Rubberduck.Parsing.VBA
         {
             _state.Status = RubberduckParserState.State.Resolving;
             var declarations = _state.AllDeclarations;
-            var tasks = _state.UnresolvedDeclarations
+            var unresolvedDeclarations = _state.UnresolvedDeclarations
                 .GroupBy(declaration => declaration.QualifiedSelection.QualifiedName)
-                .Where(grouping => grouping.Key.ComponentName != null)
-                .Select(grouping => new Task(() =>
-                {
-                    var resolver = new IdentifierReferenceResolver(grouping.Key, declarations);
-                    var listener = new IdentifierReferenceListener(resolver, token);
-                    var walker = new ParseTreeWalker();
-                    walker.Walk(listener, tree);
-                }));
-            foreach (var task in tasks)
+                .Where(grouping => grouping.Key.ComponentName != null);
+
+            Parallel.ForEach(unresolvedDeclarations, grouping =>
             {
-                task.Start();
-            }
+                var resolver = new IdentifierReferenceResolver(grouping.Key, declarations);
+                var listener = new IdentifierReferenceListener(resolver, token);
+                var walker = new ParseTreeWalker();
+                walker.Walk(listener, tree);
+            });
         }
     }
 
